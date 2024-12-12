3575b494ea2179e2d347c1f4f1f68e49bcadc52f
Adam Lukacka <adam@lukacka.com>
2024-12-12 11:10:19 +0100

    Create gallery detail routing

---
+++ app/galleries/[...slug]/page.tsx
@@ -0,0 +1,12 @@
+// app/galleries/[...slug]/page.tsx
+import GalleryDetail from "../../web/galleries/[...slug]/page";
+
+interface PageProps {
+  params: {
+    slug: string[];
+  }
+}
+
+export default function GalleryPage({ params }: PageProps) {
+  return <GalleryDetail slug={params.slug} />;
+}
