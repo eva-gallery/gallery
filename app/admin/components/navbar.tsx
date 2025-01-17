9c440eae85439f601dd309af8b4d77cb8e720ea8
LL studio <info@ll.sk>
2025-01-17 17:12:57 +0100

    user create navbar noicon

--- app/admin/components/navbar.tsx
+++ app/admin/components/navbar.tsx
@@ -14,16 +14,16 @@ const AdminNavbar: React.FC = () => {
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="/admin">
-          <Image src="/images/logo/logo-eva-gallery.svg" width="128" height="128" alt="logo" />
+          <Image src="/images/logo/logo-eva-gallery.svg" width="96" height="40" alt="logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className='justify-content-end'>
           <Nav>
             {M.modules.map((module, index) => (
-              (module !== 'Designer' && module !== 'Ai') && (
-                <Nav.Link key={index} href={`/admin/${module.toLowerCase()}`} className='text-center text-uppercase'>
-                  <Image src={`/images/icons/${module.toLowerCase()}.png`} width="64" height="64" alt={module} className='mb-2' />
-                  <br />
+              (module !== 'Designers' && module !== 'Ai') && (
+                <Nav.Link key={index} href={`/admin/${module.toLowerCase()}`} className='text-center'>
+                  {/* <Image src={`/images/icons/${module.toLowerCase()}.png`} width="64" height="64" alt={module} className='mb-2' />
+                  <br /> */}
                   {module}
                 </Nav.Link>
               )
