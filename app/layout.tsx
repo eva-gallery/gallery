48497f367da7ce0fcb14145ade915b62e1b82438
Adam Lukacka <adam@lukacka.com>
2025-01-21 14:25:17 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,63 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import { Hind } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import { config } from '@fortawesome/fontawesome-svg-core';
-config.autoAddCss = false;
-
 import 'flag-icons/css/flag-icons.min.css'
 import "./front.css";
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 
 const hind = Hind({ subsets: ["latin"], weight: ["300", "400", "500", "600", "700"] });
 
 export const metadata: Metadata = {
-  title: "EVA Gallery",
+  metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'),
+  title: {
+    template: '%s | EVA Gallery',
+    default: 'EVA Gallery'
+  },
   description: "European Visual Arts Gallery - Europes first AI-Powered Web3 Gallery for professional Artists and Gallerists.",
 };
 
-const lang: string = "sk";
-
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-
-    <html lang={lang}>
+    <html lang="en">
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MNP9SQN2');
+            `,
+          }}
+        />
+      </head>
       <body className={hind.className}>
-        {children}
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MNP9SQN2"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        <div id="app">
+          {children}
+        </div>
       </body>
     </html>
-
   );
 }
