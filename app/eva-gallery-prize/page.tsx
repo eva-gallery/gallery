82db23556701086192d8922f964f8e41e09907e8
Adam Lukacka <adam@lukacka.com>
2025-02-18 12:52:13 +0100

    Create prize page.tsx

---
+++ app/eva-gallery-prize/page.tsx
@@ -0,0 +1,23 @@
+import NavbarComponent from "@/app/web/components/NavbarComponent";
+import Footer from "@/app/web/components/Footer";
+import TermsAndConditions from "@/app/web/components/Prize";
+import Image from 'next/image';
+
+export default function PrizePage() {
+  return (
+    <>
+      <NavbarComponent />
+      
+      <div className="container py-3 py-lg-4">
+
+        <div className="row">
+          <div className="col-12">
+              <Prize />
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
