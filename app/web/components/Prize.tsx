40a9318e7cf89d7098400b767aec6b8d6515890a
Adam Lukacka <adam@lukacka.com>
2025-02-18 12:51:00 +0100

    Create Prize.tsx

---
+++ app/web/components/Prize.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Prize = () => {
+  return (
+    <div className="min-h-screen py-8 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-4xl mx-auto rounded-lg p-8">
+        <h1 className="text-3xl font-bold text-gray-900 mb-8 text-center">
+          E.V.A. Gallery Prize
+        </h1>
+
+        <div className="space-y-8">
+         
+          <section>
+           
+          </section>
+
+          <br></br>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Prize;
