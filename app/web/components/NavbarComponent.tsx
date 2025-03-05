6194763c6d81e937cf320065a10811310979bfac
Adam Lukacka <adam@lukacka.com>
2025-03-05 11:47:57 +0100

    Update NavbarComponent.tsx

--- app/web/components/NavbarComponent.tsx
+++ app/web/components/NavbarComponent.tsx
@@ -74,7 +74,7 @@ const NavbarComponent = () => {
               <Nav.Link 
                 as={Link} 
                 href="/exhibitions" 
-                className="mx-1 d-none text-dark hover:text-primary"
+                className="mx-1 text-dark hover:text-primary"
               >
                 Exhibitions
               </Nav.Link>
